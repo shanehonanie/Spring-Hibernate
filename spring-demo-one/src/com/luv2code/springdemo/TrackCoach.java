c02f2b708c897945f67bac8423837350a910fb44
Shane Honanie <shaneh425@gmail.com>
2019-07-20 16:32:21 -0700

    Bean Scopes & Lifcycles

--- spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -23,4 +23,14 @@ public class TrackCoach implements Coach {
 		// use my fortuneService to get a fortune
 		return "Just Do It: " + fortuneService.getFortune();
 	}
+	
+	// add an init method
+	public void doStartupStuff() {
+		System.out.println("TrackCoach: inside my doStartupStuff");
+	}
+	
+	// add a destroy method
+	public void doCleanupStuff() {
+		System.out.println("TrackCoach: inside my doCleanupStuff");
+	}
 }
