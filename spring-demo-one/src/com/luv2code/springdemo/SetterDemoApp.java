5a10ebbde3f43b6fd9ac9ee0778653870949dd92
Shane Honanie <shaneh425@gmail.com>
2019-07-20 14:55:49 -0700

    Dependency Injection with XML

--- spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -2,24 +2,27 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloSpringApp {
+public class SetterDemoApp {
 
 	public static void main(String[] args) {
 		
-		// load the spring config file
+		// load spring configuration file
 		ClassPathXmlApplicationContext context =
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// retrieve bean from spring container
-		Coach theCoach = context.getBean("myCoach", Coach.class);
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
-		// call methods on the bean
+		// call methods on bean
 		System.out.println(theCoach.getDailyWorkout());
-		
-		// call fortune method
 		System.out.println(theCoach.getDailyFortune());
+		
+		// Call the new methods to get literal data
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
 		
 		// close the context
 		context.close();
 	}
+
 }
--- spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -1,10 +1,26 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	// define a private field for dependency
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {
+		
+	}
+	
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Run a hard 5k";
 	}
 
+	@Override
+	public String getDailyFortune() {
+		// use my fortuneService to get a fortune
+		return "Just Do It: " + fortuneService.getFortune();
+	}
 }
