1d8cc8143d5e4bea13a2fe9ccc87d8a3b20134a0
Shane Honanie <shaneh425@gmail.com>
2019-07-24 16:03:34 -0700

    Custom Validation Rules

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
+
 public class Customer {
 
 	private String firstName;
@@ -17,10 +19,13 @@ public class Customer {
 	@NotNull(message="is required")
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="must be less than or equal to 10")
-	private Integer freePasses; // use Integer instead of int for String -> Integer conversion
+	private Integer freePasses; // use Integer instead of int for String -> Integer conversion & NULL
 
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode;
+	
+	@CourseCode(value="TOPS", message="must start with TOPS")
+	private String courseCode;
 	
 	public String getPostalCode() {
 		return postalCode;
@@ -52,5 +57,13 @@ public class Customer {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
 	}	
 }
