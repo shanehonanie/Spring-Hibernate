2dfe7845c089cd7a4562db0c71cfe4e72d87abdf
Shane Honanie <shaneh425@gmail.com>
2019-08-03 13:31:59 -0700

    AOP Aspects Ordering

---
+++ spring-demo-aop-pointcut-aspects-order/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,16 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterOrSetter()")
+	public void addAccountAdvice() {
+		System.out.println("\n=====>>> MyDemoLoggingAspect()");
+	}
+}
