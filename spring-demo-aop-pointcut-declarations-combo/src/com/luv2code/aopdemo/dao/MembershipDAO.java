ee5417dbfe88557b4f4268feceb963aa7ff8e3a1
Shane Honanie <shaneh425@gmail.com>
2019-08-02 19:04:02 -0700

    AOP Pointcut Declarations

---
+++ spring-demo-aop-pointcut-declarations-combo/src/com/luv2code/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,16 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipDAO {
+	public boolean addAccount() {
+		System.out.println(getClass() + ": DOING STUFF: ADDING A MEMBERSHIP ACCOUNT");
+		
+		return true;
+	}
+	
+	public void goToSleep() {
+		System.out.println(getClass() + ": Going to Sleep...");
+	}
+}
